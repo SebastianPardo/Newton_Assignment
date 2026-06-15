845c3e77c10250b0fb3efe313b77fc4a7f950ce3
Sebastian Pardo <js.pardo.j@gmail.com>
2026-06-15 14:14:07 -0400

    Added genre and platform services

---
+++ VideoGamesCatalogue/NewtonServices/Controllers/GenreController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewtonServices.Bussines.Interfaces;
+using NewtonServices.Models.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewtonServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        IGenresService _genresService;
+        public GenreController(IGenresService genresService)
+        {
+            _genresService = genresService;
+        }
+        // GET: api/<PlatformController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Genre>> Get()
+        {
+            var genres = _genresService.GetAll();
+            if (genres == null)
+            {
+                return NotFound();
+            }
+            return Ok(genres);
+        }
+    }
+}
---
+++ VideoGamesCatalogue/NewtonServices/Controllers/PlatformController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewtonServices.Bussines.Interfaces;
+using NewtonServices.Models.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewtonServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        IPlatformsService _platformsService;
+        public PlatformController(IPlatformsService platformsService)
+        {
+            _platformsService = platformsService;
+        }
+        // GET: api/<PlatformController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Platform>> Get()
+        {
+            var platforms = _platformsService.GetAll();
+            if(platforms == null) {
+                return NotFound();
+            }
+            return Ok(platforms);
+        }
+    }
+}
